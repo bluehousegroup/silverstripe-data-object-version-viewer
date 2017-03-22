3eb85f6cf82831188ce770ae3ac53826d1551ea8
Joe Siebert <joe@bluehousegroup.com>
2017-03-22 18:14:07 -0400

    Fixing History form for GridFields

--- code/VersionedRevertDODetailsForm.php
+++ code/VersionedRevertDODetailsForm.php
@@ -2,7 +2,7 @@
 
 class VersionedRevertDODetailsForm extends Extension {
 
-	public function updateItemEditForm($form) {
+    public function updateItemEditForm($form) {
 
         $form->Actions()->push(
             FormAction::create(
@@ -10,11 +10,23 @@ class VersionedRevertDODetailsForm extends Extension {
                 'History'
                 )
             );
-	}
+    }
 
     public function goHistory($data, $form) {
-        $url = str_replace('/EditForm/', '/HistoryForm/', $this->owner->Link());
-		$controller = Controller::curr();
+        //get the url parts
+        $parts = explode('/', $data['url']);
+        $model_name = $parts[6];
+        $item = $parts[8];
+
+        //De-pluraize the model name if it is plural
+        if(substr($model_name, -1) == 's')
+            $model_name = substr($model_name, 0, -1);
+
+        //manually construct the url
+        $url = 'admin/' . strtolower($model_name) . '/' . $model_name . '/HistoryForm/field/' . $model_name . '/item/' . $item . '';
+
+        // $url = str_replace('/EditForm/', '/HistoryForm/', $this->owner->Link());
+        $controller = Controller::curr();
         $controller->getResponse()->addHeader("X-Pjax", "Content");
         $controller->redirect(Controller::join_links($url, 'history'));
     }
